3e9393cc86e19336460807cab16abdeec608da85
George Beckert <gbeckert@stagefronttickets.com>
2020-03-03 20:22:20 -0500

    Login Page Frontend Built

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,48 @@
 // src/App.js
-import React from "react";
+import React from 'react';
+import flowers from './images/flowers.png';
 
 function App() {
   return (
-    <div className="flex flex-col w-3/4 mx-auto my-12 items-center">
-      <h1 className="font-bold text-xl mb-2">TailWind Starter</h1>
-			<p className="text-gray-700 text-base">Base project with typescirpt and tailwind for react</p>
+    <div
+      className="h-screen w-full bg-white"
+      style={{
+        backgroundImage: `
+        linear-gradient(to bottom, rgba(253, 252, 252, 1), rgba(253,252,252, 0.7)),
+        url(${flowers})
+        `,
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center bottom',
+      }}
+>
+      <h1 className="font-display font-bold text-6xl mb-2 leading-tight px-5 pt-10 text-brown-200">
+        Music Bookclub
+      </h1>
+      <form className="pl-5 pr-12 absolute bottom-0 mb-32 w-full">
+        <div className="mb-4">
+          <label className="font-display block text-gray-700 text-lg font-bold text-brown-200" htmlFor="username">
+            Name
+          </label>
+          <input
+            className="appearance-none border border-black rounded w-full py-2 px-3 leading-tight bg-white"
+            id="username"
+            type="text"
+            placeholder="Username"
+          />
+        </div>
+        <div className="mb-6">
+          <label className="block text-gray-700 text-sm font-bold mb-2 text-brown-200" htmlFor="password">
+            Password
+          </label>
+          <input
+            className="appearance-none border border-black rounded w-full py-2 px-3 leading-tight"
+            id="password"
+            type="password"
+            placeholder="******************"
+          />
+        </div>
+      </form>
     </div>
-
   );
 }
 
---
+++ src/login/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+interface props {
+}
+
+const LoginScreen:React.FC<props> = ()=>{
+  return <div>hi</div>
+}
+
+
+
